96f50d06ebedaa3fff17b075daab7441e4a59d2b
Steven Lamp <vxn3kr@virginia.edu>
2025-02-28 11:44:01 -0500

    addded seperate models

---
+++ agents/default-phi.py
@@ -0,0 +1,30 @@
+import asyncio
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from browser_use import Agent
+from dotenv import load_dotenv
+import logging
+import random
+import time
+
+# Load environment variables
+load_dotenv()
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+async def main():
+    agent = Agent(
+        task="Imagine that you are a real human, and that you have access to a computer. Perform real actions on that computer such as making searches on google, browsing youtube, and shopping online. You can imagine you are any gender. Make sure to take breaks and sleep. ",
+        llm=ChatOllama(model="phi4:14b"),
+        use_vision=True, 
+    )
+    
+    
+    result = await agent.run()
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
